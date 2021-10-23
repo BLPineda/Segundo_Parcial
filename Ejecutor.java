6c5394fdf3f83c3657d3bbb7e30948ec1c6d0c61
Your Name <you@example.com>
2021-10-22 20:14:32 -0600

    Segundo parcial programación II

---
+++ Ejecutor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Segundo_Parcial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenin
+ */
+public class Ejecutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Datos datos = new Datos();
+    
+    public static void main(String[] args) {
+        datos.Datos_persona();
+        datos.Mostrar_datos();
+    }
+    
+}
